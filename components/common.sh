ea73e17975b80af48f2907f69ffb4bf7268e4314
chakra <chakradhar06@gmail.com>
2022-03-27 12:04:59 -0400

    Created print function

--- components/common.sh
+++ components/common.sh
@@ -2,14 +2,13 @@
 
 USER_ID=$(sudo id -u)
 if [ ${USER_ID} -ne 0 ]; then
-  echo "\e[36m You need to be root user to run the script"
+  echo "\e[36m You need to be root user to run the script\e[0m"
   exit 2
 fi
 
 LOG_FILE=/tmp/jenkins.log
 rm -rf $LOG_FILE
 
-if []
 Print() {
   echo -e "\e[36m $1 \e[0m"
 }
