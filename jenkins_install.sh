f371e126a09b6b90fb5e414dbfa8f81958e4c23b
chakra <chakradhar06@gmail.com>
2022-03-27 12:05:48 -0400

    Created print function

--- jenkins_install.sh
+++ jenkins_install.sh
@@ -6,7 +6,6 @@ Print "Configure repos"
 curl -L https://pkg.jenkins.io/redhat-stable/jenkins.repo -o /etc/yum.repos.d/jenkins.repo
 StatCheck $?
 
-
 Print "Validate repos key"
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 StatCheck $?
