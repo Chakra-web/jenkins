009cb18c7eb9830adedb35f97072ec48992d81aa
chakra <chakradhar06@gmail.com>
2022-03-27 11:35:47 -0400

    Created print function

--- components/common.sh
+++ components/common.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+LOG_FILE=/tmp/jenkins.log
+rm -rf $LOG_FILE
 Print() {
   echo -e "\e[36m $1 \e[0m"
+}
+
+StatCheck() {
+  if [ $? -ne 0 ]; then
+    echo -e "\e[31mFAILURE\e[0m"
+    exit 1
+  else
+    echo -e "\e[32mSUCCESS\e[0m"
+  fi
 }
--- jenkins_install.sh
+++ jenkins_install.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+source components/common.sh
+
+Print "Configure repos"
+curl -L https://pkg.jenkins.io/redhat-stable/jenkins.repo -o /etc/yum.repos.d/jenkins.repo
+StatCheck $?
+
+
+Print "Validate repos key"
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+StatCheck $?
+
+Print "Install java"
+yum install fontconfig java-11-openjdk
+StatCheck $?
+
+Print "install jenkins"
+yum install jenkins
+StatCheck $?
